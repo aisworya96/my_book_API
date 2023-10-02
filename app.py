d91767ac145749a1615b4de32360be097007616c
aisworya96 <paisworya2@gmail.com>
2023-10-02 12:05:02 +0530

    My First Commit

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 books = [
     {"id": 1, "title": "Book 1", "author": "Author 1"},
+
     {"id": 2, "title": "Book 2", "author": "Author 2"},
     {"id": 3, "title": "Book 3", "author": "Author 3"},
     {"id": 4, "title": "Book 4", "author": "Author 4"},
